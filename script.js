6c2b6f1d18a6375e45094ceca19269d8e43a5c3f
Pedro <pedro.h.silva522@aluno.senai.br>
2025-06-12 04:15:48 -0300

    Correção

--- script.js
+++ script.js
@@ -9,10 +9,17 @@ if(nome==""){
 }
 else{
     titulo.innerHTML = `Lista de Tarefas do(a) ${nome}`
+    tarefa.focus();
 }
 addBtn.addEventListener("click", criarTarefa);
 
+tarefa.addEventListener('keypress', function (e) {
+    if(e.key == 'Enter') criarTarefa()
+})
 function criarTarefa(){
+    if(tarefa.value== ""){
+        alert("digite o nome de sua tarefa")
+    } else{
     const listItem = document.createElement('li')
     listItem.textContent = tarefa.value;
     taskList.appendChild(listItem);
@@ -36,10 +43,11 @@ let buttonsItem= document.createElement('div')
 buttonsItem.classList.toggle('buttonsItem')
 listItem.appendChild(buttonsItem)
 buttonsItem.appendChild(concluirButton)
-buttonsItem.appendChild(removeButton
+buttonsItem.appendChild(removeButton)
 
-)
+tarefa.value =''
+tarefa.focus();
 concluirButton.addEventListener("click", function(){
     listItem.style.textDecoration = "line-through";
 })
-}
+}}
