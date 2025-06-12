9ab562d5073dafd232f11e03617d040eb5dd90a2
Pedro <pedro.h.silva522@aluno.senai.br>
2025-06-12 03:32:13 -0300

    correção css

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-//definir constantes para uso no codigo//
-const addBtn = document.getElementById('btn-add')
-const tarefa = document.getElementById('tarefa')
-const taskList = document.getElementById('taskList')
-const titulo = document.getElementById('titulo')
+const addBtn = document.getElementById("addBtn");
+const tarefa = document.getElementById("tarefa");
+const taskList = document.getElementById("taskList")
+const titulo = document.getElementById("titulo")
 
 let nome = prompt("Qual o seu nome?")
-
-titulo.innerHTML = ` Lista de tarefa: ${nome}`
-//acompanha o evento de clique do botão adicior tarefa
-addBtn.addEventListener("click",criaTarefa)
-
-function criaTarefa(){
-    
+if(nome==""){
+    titulo.innerHTML = 'Lista de Tarefas' 
 }
+else{
+    titulo.innerHTML = `Lista de Tarefas do(a) ${nome}`
+}
+addBtn.addEventListener("click", criarTarefa);
 
+function criarTarefa(){
+    const listItem = document.createElement('li')
+    listItem.textContent = tarefa.value;
+    taskList.appendChild(listItem);
+
+//criar botão de deletar tarefa
+const removeButton = document.createElement('button')
+removeButton.id="remove"
+removeButton.textContent = "X"
+
+removeButton.addEventListener("click", function(){
+    taskList.removeChild(listItem)
+    
+})
+//criar botão de concluir tarefa
+const concluirButton = document.createElement('button')
+concluirButton.id ="conclui"
+concluirButton.textContent ="✓"
+
+//listen.appendChild(concluirButton)
+let buttonsItem= document.createElement('div')
+buttonsItem.classList.toggle('buttonsItem')
+listItem.appendChild(buttonsItem)
+buttonsItem.appendChild(concluirButton)
+buttonsItem.appendChild(removeButton
+
+)
+concluirButton.addEventListener("click", function(){
+    listItem.style.textDecoration = "line-through";
+})
+}
